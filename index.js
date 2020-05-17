955b0e978c0bd6e204c02e25693e23cbff339fe4
samya shajrawi <samiashajrawi@gmail.com>
2020-05-17 02:15:09 -0700

    Node Callbacks and Error Handling

---
+++ index.js
@@ -0,0 +1,20 @@
+var rect = require("./rectangle");
+
+function solveRect(l,b) {
+    console.log("Solving for rectangle with l = " + l + " and b = " + b);
+
+    rect(l, b, (err, rectangle) => {
+        if (err) {
+            console.log("Error : " + err.message);
+        } else {
+            console.log("The area of the rectangle  l =  " + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle  l =  " + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+   console.log("Call is done");
+}
+
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,5);
+solveRect(-3,5);
